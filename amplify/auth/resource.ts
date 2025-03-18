721897db0e515cce73481df2d8dcd4189961e643
pisuthd <pisuth.dae@gmail.com>
2025-03-18 21:51:00 +0900

    Add Google Authentication

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,16 @@ import { defineAuth } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        'http://localhost:3000/dashboard',
+        'https://www.tamagolabs.com/dashboard',
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://www.tamagolabs.com'],
+    }
   },
 });
