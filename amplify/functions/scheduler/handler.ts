f5afd5b2df5bb1d1c3e29b9cffca896a64fa008d
pisuthd <pisuth.dae@gmail.com>
2025-03-25 22:12:58 +0900

    Add automation for Cronos agent

--- amplify/functions/scheduler/handler.ts
+++ amplify/functions/scheduler/handler.ts
@@ -9,6 +9,9 @@ import { ChatAnthropic } from "@langchain/anthropic"
 import { AgentRuntime, LocalSigner, createAptosTools } from "move-agent-kit"
 import { Account, SigningSchemeInput, Aptos, AptosConfig, Ed25519PrivateKey, Secp256k1PrivateKey, Network, PrivateKey, PrivateKeyVariants } from "@aptos-labs/ts-sdk"
 import { createReactAgent } from "@langchain/langgraph/prebuilt"
+import { CronosAgent, createCronosTools } from "../../../lib"
+import { CronosZkEvm, CronosEvm } from '@crypto.com/developer-platform-client';
+
 
 const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
 
@@ -17,6 +20,13 @@ const llm = new ChatAnthropic({
     model: "claude-3-5-sonnet-latest",
     apiKey: process.env.ANTHROPIC_API_KEY
 })
+
+
+const CRONOS_ZKEVM_TESTNET_API_KEY = process.env.CRONOS_ZKEVM_TESTNET_API_KEY || ""
+const CRONOS_ZKEVM_API_KEY = process.env.CRONOS_ZKEVM_API_KEY || ""
+const CRONOS_EVM_TESTNET_API_KEY = process.env.CRONOS_EVM_TESTNET_API_KEY || ""
+const CRONOS_EVM_API_KEY = process.env.CRONOS_EVM_API_KEY || ""
+
 
 const client = generateClient<Schema>()
 
@@ -56,7 +66,12 @@ export const handler: EventBridgeHandler<"Scheduled Event", null, void> = async 
 
             if (need_update) {
                 console.log(`Processing agent: ${agent.id}...`);
-                await runAgent(agent);
+                if (agent.blockchain === "aptos") {
+                    await runAptosAgent(agent);
+                } else if (agent.blockchain === "cronos") {
+                    await runCronosAgent(agent)
+                }
+
             } else {
                 console.log(`No update needed for agent: ${agent.id}.`);
             }
@@ -68,7 +83,7 @@ export const handler: EventBridgeHandler<"Scheduled Event", null, void> = async 
 
 }
 
-const runAgent = async (agent: Schema["Agent"]["type"]) => {
+const runAptosAgent = async (agent: Schema["Agent"]["type"]) => {
 
     const wallets = await agent.wallets()
     const wallet: any = wallets.data[0]
@@ -165,12 +180,94 @@ const runAgent = async (agent: Schema["Agent"]["type"]) => {
 
 }
 
+const runCronosAgent = async (agent: Schema["Agent"]["type"]) => {
+
+    const wallets = await agent.wallets()
+    const wallet: any = wallets.data[0]
+
+    // Init Cronos config
+    const cronos = new CronosAgent(
+        wallet.key,
+        agent.subnetwork === "evm" ? (agent.isTestnet ? CronosEvm.Testnet : CronosEvm.Mainnet) : (agent.isTestnet ? CronosZkEvm.Testnet : CronosZkEvm.Mainnet),
+        agent.subnetwork === "evm" ? (agent.isTestnet ? CRONOS_EVM_TESTNET_API_KEY : CRONOS_EVM_API_KEY) : (agent.isTestnet ? CRONOS_ZKEVM_TESTNET_API_KEY : CRONOS_ZKEVM_API_KEY),
+        process.env.ANTHROPIC_API_KEY || ""
+    )
+    const tools = createCronosTools(cronos)
+
+    // Create React agent
+    const reactAgent = createReactAgent({
+        llm,
+        tools,
+        messageModifier: `
+        You are a helpful AI agent that can interact on-chain with the Cronos blockchain. 
+        You can perform various blockchain operations using your available tools. 
+        If you ever need funds, you can request them from the faucet. If not, you can provide your wallet details and request funds from the user.
+        If an internal (5XX) error occurs, inform the user and suggest trying again later. 
+        If a requested action is not supported by your current tools, acknowledge the limitation and encourage the user to extend your capabilities. 
+        Keep responses concise and useful. Avoid restating tool descriptions unless explicitly requested.
+      `,
+    })
+
+    let messages: any = agent?.messages ? JSON.parse(String(agent.messages)) : []
+
+    console.log("Existing messages : ", messages)
+
+    const userPrompt = {
+        role: 'user',
+        content: agent.promptInput
+    }
+
+    const output = await reactAgent.invoke(
+        {
+            messages: [...messages, userPrompt]
+        }
+    )
+
+    const dataResultMessage: any = extractOnlyLastMessage(output)
+
+    if (dataResultMessage && dataResultMessage.role === "assistant" && dataResultMessage.content) {
+
+        const executePrompt = {
+            role: 'user',
+            content: [
+                "Based on the following condition:",
+                `${agent.promptDecision}`,
+                "Execute the following if the condition is met:",
+                `${agent.promptExecute}`,
+            ].join("\n")
+        }
+
+        const finalOutput = await reactAgent.invoke(
+            {
+                messages: [...messages, userPrompt, dataResultMessage, executePrompt]
+            }
+        )
+
+        // Override old messages
+        const finalized = parseLangchain(finalOutput.messages).map((msg: any) => {
+            const message = [...messages, userPrompt, dataResultMessage, executePrompt].find((i: any) => i.id === msg.id)
+            if (message) {
+                msg = message
+            }
+            return msg
+        })
+
+        console.log("saving messages :", finalized)
+        await client.models.Agent.update({
+            id: agent.id,
+            messages: JSON.stringify(finalized),
+            lastRanAt: Math.floor(new Date().valueOf() / 1000)
+        })
+
+    }
+
+}
+
 const extractOnlyLastMessage = (output: any) => {
 
     let last
 
     output.messages.map((msg: any) => {
-        // const role = msg.additional_kwargs?.role || (msg.role || "user")
         if (msg?.tool_call_id) {
 
         } else {
--- amplify/functions/scheduler/resource.ts
+++ amplify/functions/scheduler/resource.ts
@@ -7,7 +7,11 @@ export const scheduler = defineFunction({
     resourceGroupName: "data",
     entry: './handler.ts',
     environment: {
-        ANTHROPIC_API_KEY: secret('ANTHROPIC_API_KEY')
+        ANTHROPIC_API_KEY: secret('ANTHROPIC_API_KEY'),
+        CRONOS_ZKEVM_TESTNET_API_KEY: secret("CRONOS_ZKEVM_TESTNET_API_KEY"),
+        CRONOS_ZKEVM_API_KEY: secret("CRONOS_ZKEVM_API_KEY"),
+        CRONOS_EVM_TESTNET_API_KEY: secret("CRONOS_EVM_TESTNET_API_KEY"),
+        CRONOS_EVM_API_KEY: secret("CRONOS_EVM_API_KEY")
     },
     timeoutSeconds: 600
 })
