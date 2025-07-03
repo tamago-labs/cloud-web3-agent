52e927c9c05f7e9152fc53c283305cb699c163f3
pisuthd <pisuth.dae@gmail.com>
2025-07-03 18:09:39 +0900

    Update login screen

---
+++ app/account/layout.tsx
@@ -0,0 +1,23 @@
+
+
+import { Providers } from "../providers";
+import { Inter } from "next/font/google";
+
+const InterFont = Inter({
+    weight: ["400", "500", "700"],
+    subsets: ["latin"],
+});
+
+export default function DashboardLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <Providers>
+            <div className={`${InterFont.className} min-h-screen flex flex-col `}>
+                {children}
+            </div>
+        </Providers>
+    )
+}
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,22 +2,16 @@
 
 import { useEffect, useState } from 'react';
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 import { Authenticator, useTheme, View, Heading, Image, Text, Button, ThemeProvider, Theme } from '@aws-amplify/ui-react';
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
-import { Divide, Menu, X } from "react-feather"
-import { PropsWithChildren } from 'react';
 
-import Loading from '@/components/Loading';
 import outputs from "@/amplify_outputs.json";
 import { usePathname } from 'next/navigation'
 import Link from "next/link"
 
-import CloudAgentProvider from "../hooks/useCloudAgent"
+// import CloudAgentProvider from "../hooks/useCloudAgent"
 
 Amplify.configure(outputs);
 
@@ -27,13 +21,11 @@ const components = {
 
         return (
             <View textAlign="center" padding={tokens.space.large}>
-                <Link href="/" className="inline-flex">
-                    <div className="relative px-4 py-2 bg-gradient-to-tr from-blue-600 to-purple-600 rounded-md flex items-center shadow-lg shadow-purple-900/20">
-                        <div className="absolute inset-0 bg-black opacity-20 rounded-md"></div>
-
-                        <h1 className="font-bold text-white text-lg relative z-10">Tamago Labs</h1>
-                    </div>
-                </Link>
+                <Link href="/" className="inline-flex mb-[20px] mt-[20px]">
+                    <span className="text-gray-900 text-xl font-bold font-mono">
+                        [tamago_labs]
+                    </span>
+                </Link> 
             </View>
         );
     },
@@ -42,7 +34,7 @@ const components = {
 
         return (
             <View textAlign="center" padding={tokens.space.large}>
-                <Text color={tokens.colors.white}>
+                <Text color={tokens.colors.black}>
                     Secured by AWS Cognito
                 </Text>
             </View>
@@ -52,26 +44,6 @@ const components = {
 
 export function Providers({ children }: any) {
 
-    const [showLoader, setShowLoader] = useState(true);
-
-    const pathname = usePathname()
-
-    useEffect(() => {
-        AOS.init({
-            once: true,
-        });
-    }, []);
-
-    useEffect(() => {
-
-        const screenLoader = document.getElementsByClassName('screen_loader');
-        if (screenLoader?.length) {
-            setTimeout(() => {
-                setShowLoader(false);
-            }, 200);
-        }
-
-    });
 
     const { tokens } = useTheme()
 
@@ -96,19 +68,13 @@ export function Providers({ children }: any) {
     }
 
     return (
-        <CloudAgentProvider>
-            {showLoader ? (
-                <Loading />
-            ) :
-                <ThemeProvider theme={theme} >
-                    <View className="min-h-screen relative ">
-                        <Authenticator components={components}>
-                            {children}
-                        </Authenticator>
-                    </View>
-                </ThemeProvider>
-            }
-        </CloudAgentProvider>
+        <ThemeProvider theme={theme} >
+            <View className="min-h-screen relative ">
+                <Authenticator components={components}>
+                    {children}
+                </Authenticator>
+            </View>
+        </ThemeProvider>
     );
 }
 
