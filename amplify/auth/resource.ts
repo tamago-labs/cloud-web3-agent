43e8d29687bcaea05ccf044bab5f9d1afdee1d39
pisuthd <pisuth.dae@gmail.com>
2025-07-13 21:35:49 +0900

    Remove Google Auth for now

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,16 +7,16 @@ import { defineAuth, secret } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET')
-      },
-      callbackUrls: [
-        'http://localhost:3000/dashboard',
-        'https://www.tamagolabs.com/dashboard',
-      ],
-      logoutUrls: ['http://localhost:3000/', 'https://www.tamagolabs.com'],
-    }
+    // externalProviders: {
+    //   google: {
+    //     clientId: secret('GOOGLE_CLIENT_ID'),
+    //     clientSecret: secret('GOOGLE_CLIENT_SECRET')
+    //   },
+    //   callbackUrls: [
+    //     'http://localhost:3000/dashboard',
+    //     'https://www.tamagolabs.com/dashboard',
+    //   ],
+    //   logoutUrls: ['http://localhost:3000/', 'https://www.tamagolabs.com'],
+    // }
   },
 });
