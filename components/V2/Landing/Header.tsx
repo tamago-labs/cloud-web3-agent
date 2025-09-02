dd6c2edb9f2e19c5bba9da685618994a6fda6446
pisuthd <pisuth.dae@gmail.com>
2025-09-02 18:56:09 +0900

    new logo for bohdi tree

--- components/V2/Landing/Header.tsx
+++ components/V2/Landing/Header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 
-import Link from "next/link" 
+import Link from "next/link"
 import { useEffect, useContext, useState } from "react";
 import { AccountContext } from "@/contexts/account";
 
@@ -13,7 +13,7 @@ const Header = ({ bgColor }: any) => {
 
     useEffect(() => {
         const currentDomain = window.location.origin
-        setCurrentDomain(currentDomain.includes("tamagolabs.com") ? "tamago_labs" : "bohdi_tree"); // or window.location.hostname
+        setCurrentDomain(currentDomain.includes("tamagolabs.com") ? "tamago_labs" : "bodhi_tree"); // or window.location.hostname
     }, []);
 
     return (
@@ -21,13 +21,24 @@ const Header = ({ bgColor }: any) => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
                     {/* Logo */}
-                    <div className="flex justify-start lg:w-0 lg:flex-1">
-                        <Link href="/" className="flex items-center">
-                            <span className="text-gray-900 text-xl font-bold font-mono">
-                                [{currentDomain}]
-                            </span>
-                        </Link>
-                    </div>
+                    {currentDomain === "tamago_labs" && (
+                        <div className="flex justify-start lg:w-0 lg:flex-1">
+                            <Link href="/" className="flex items-center">
+                                <span className="text-gray-900 text-xl font-bold font-mono">
+                                    [{currentDomain}]
+                                </span>
+                            </Link>
+                        </div>
+                    )}
+
+                    {currentDomain === "bodhi_tree" && (
+                        <div className="flex justify-start lg:w-0 lg:flex-1">
+                            <Link href="/" className="flex items-center">
+                                <img src="./assets/images/logo-bodhi_tree.png" className="w-[190px] md:w-[230px]" />
+                            </Link>
+                        </div>
+                    )}
+
 
                     {/* Desktop navigation */}
                     <nav className="hidden md:flex space-x-2 md:space-x-8">
@@ -42,7 +53,7 @@ const Header = ({ bgColor }: any) => {
                         </Link>
                         <Link href="/server-status" className="text-gray-600 hover:text-gray-900 font-medium transition-colors">
                             Status
-                        </Link> 
+                        </Link>
                     </nav>
 
                     {/* CTA Buttons */}
